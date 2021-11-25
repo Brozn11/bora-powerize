90e0bfdc3590df608592b1a157d7684469fb88c5
Albert <wi18b088@technikum-wien.at>
2021-11-25 18:57:15 +0000

    fixed imports

--- src/MathStuff.java
+++ src/MathStuff.java
@@ -1,5 +1,4 @@
 //# BEGIN SKELETON
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -213,7 +212,7 @@ public abstract class MathStuff {
 
     public static ArrayList<Integer> primeFactors(int number) {
         int n = number;
-        List<Integer> factors = new ArrayList<Integer>();
+        ArrayList<Integer> factors = new ArrayList<Integer>();
         for (int i = 2; i <= n; i++) {
             while (n % i == 0) {
                 factors.add(i);
